259063226f2be15e0ac16b960950024544d83196
Md Arik Rayhan <mdarikrayhan@gmail.com>
2022-09-25 12:50:09 +0600

    demo change

--- header.php
+++ header.php
@@ -32,7 +32,7 @@ if(!isset($menu)){
 	<div class="banner-rs">
 		<header>
 			<nav class="navbar navbar-expand-lg navbar-light bg-light">
-				<a class="navbar-brand" href="index.php">ATS Bangladesh</a>
+				<a class="navbar-brand" href="index.php">ATS Bangladesh 2</a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
 				    aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon">
